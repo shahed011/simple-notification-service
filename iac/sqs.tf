a588dce2e7d4928d417d0f4fde47468a3b2e04ff
Muhammad Shahed Aziz <shahed011@hotmail.com>
2022-12-30 23:06:00 +0000

    Sqs vis timeout update

--- iac/sqs.tf
+++ iac/sqs.tf
@@ -1,9 +1,9 @@
 resource "aws_sqs_queue" "service_queue" {
-  name                      = "simple-notification-service-queue"
-  delay_seconds             = 90
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-  visibility_timeout_seconds = 300
+  name                       = "simple-notification-service-queue"
+  delay_seconds              = 90
+  message_retention_seconds  = 86400
+  receive_wait_time_seconds  = 10
+  visibility_timeout_seconds = 600
 
   tags = {
     Name = "simple-notification-service-sqs"
